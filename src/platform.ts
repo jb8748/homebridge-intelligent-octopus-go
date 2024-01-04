a4d71247d8e1f8c0aa265ade47a6e19c6cae0ccb
James Ball <me@jamesball.co.uk>
2024-01-04 22:04:07 +0000

    Create my own class for Platform accessories

--- src/platform.ts
+++ src/platform.ts
@@ -1,7 +1,7 @@
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
 
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { ExamplePlatformAccessory } from './platformAccessory';
+import { IntelligentOctopusGoPlatformAccessory } from './platformAccessory';
 
 /**
  * HomebridgePlatform
@@ -93,7 +93,7 @@ export class IntelligentOctopusGoPlatform implements DynamicPlatformPlugin {
 
         // create the accessory handler for the restored accessory
         // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, existingAccessory);
+        new IntelligentOctopusGoPlatformAccessory(this, existingAccessory);
 
         // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
         // remove platform accessories when no longer present
@@ -112,7 +112,7 @@ export class IntelligentOctopusGoPlatform implements DynamicPlatformPlugin {
 
         // create the accessory handler for the newly create accessory
         // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
+        new IntelligentOctopusGoPlatformAccessory(this, accessory);
 
         // link the accessory to your platform
         this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
